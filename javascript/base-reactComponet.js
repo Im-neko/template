7ad61d798a518440e44062102621ee3b1ad3ace0
im-neko <lukepuyuki0430@gmail.com>
2018-09-22 23:19:14 +0900

    fix to format

--- javascript/base-reactComponet.js
+++ javascript/base-reactComponet.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 
 
 export default class extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
   render () {
     return (
       <div>
