5fd7c2622c14c41e386812e4f1cd912df2684c82
Im-neko <lukepuyuki0430@gmail.com>
2018-09-29 00:18:09 +0900

    update template

--- python/base-defaultClass.py
+++ python/base-defaultClass.py
@@ -10,3 +10,16 @@ class {{_expr_:substitute('{{_input_:name}}', '\w\+', '\u\0', '')}}(object):
 
     def __repr__(self):
         return
+
+    @classmethod
+    def public_class_method(cls):
+        """
+        インスタンス化しなくても呼び出せる
+        """
+        pass
+
+
+
+if __name__ == '__main__':
+    ins = {{_expr_:substitute('{{_input_:name}}', '\w\+', '\u\0', '')}}()
+    ins.public_class_method()
