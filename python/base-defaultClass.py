a3875a3002be85bef61d42a6a4d42f613dd70269
Im-neko <lukepuyuki0430@gmail.com>
2018-09-29 00:02:21 +0900

    add class template

---
+++ python/base-defaultClass.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding:utf8 -*-
+"""
+{{_name_}}
+"""
+
+class {{_expr_:substitute('{{_input_:name}}', '\w\+', '\u\0', '')}}(object):
+
+	def __init__(self{{_cursor_}}):
+
+    def __repr__(self):
+        return
